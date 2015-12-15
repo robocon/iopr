8600d4ef974edf3902a8534f46e043dfa6f7df7f
Kritsanasak Kuntaros <roboconk@gmail.com>
2015-12-15 23:09:12 +0700

    - ลบ หน้าแอดมินที่ซอ้นกัน

--- modules/admin/admin/upload.php
+++
@@ -1,7 +0,0 @@
-<?
-CheckAdmin($_SESSION['admin_user'], $_SESSION['admin_pwd']);
-?>
-<link href="css/template_css.css" rel="stylesheet" type="text/css" />
-
-<? 
-				include('modules/uploads/index.php');?>
